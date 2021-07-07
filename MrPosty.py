03716fa3e4d6bc24580cd5584c47aa0547729616
Steven Lamp <42517762+LampSteven17@users.noreply.github.com>
2021-07-07 15:28:12 -0400

    Huge update for version that works with the new website

--- MrPosty.py
+++ MrPosty.py
@@ -4,7 +4,6 @@
 import pandas as pd
 import time
 import re
-import requests
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,31 +11,119 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import Select
 
 # PUT NAME OF FILE HERE ########################################################
 FILENAME = "ESCAPEAllDataSingleValue.csv"
 ################################################################################
 
 GWTG = "https://www.mdcalc.com/gwtg-heart-failure-risk-score"
+MAGGIC = "http://www.heartfailurerisk.org/"
 
 def main():
 
     daty = getDataFromCsv()
 
-    renderScoresGWTG(daty)
+    #renderScoresGWTG(daty)
+    renderScoresMAGGIC(daty)
 
     print(daty)
     daty.to_csv("OUTFILE.csv")
-
-
-
-
 
 def getDataFromCsv():
     frame = pd.read_csv(FILENAME, header = 0)
     return frame.replace(r'^\s*$', np.nan, regex=True)
 
 
+
+
+
+############################# MAGGIC ###########################################
+def WTH(x):
+    return {
+        1: 0,
+        2: 2,
+        3: 6,
+        4: 8
+    }.get(x, 0)
+
+def renderScoresMAGGIC(datas):
+    driver = webdriver.Firefox() #SWITCH TO: Chrome() IF NEEDED
+    driver.get(MAGGIC)
+
+    #CLICK THE ACCEPT BUTTON: #accept-terms
+    driver.find_element_by_css_selector('#accept-terms').click()
+
+
+    rows = datas.shape[0]
+    results = []
+
+############## CHANGE FROM ROWS IF NEEDED
+    for i in range(15):
+
+        res = parseMAGGIC(driver,
+                          str(datas['Age'][i]),
+                          (datas['Gender'][i] - 1),
+                          datas['DIAB'][i],
+                          datas['COPD'][i],
+                          (0 if datas['SMOKING'][i] < 1 else 1),
+                          str(WTH(datas['NYHA'][i]))
+                          )
+
+        print("MAGGIC: " + res)
+
+        results.append(res)
+
+
+
+    #driver.quit()
+    if (len(results) != rows):
+        for i in range (rows - len(results)):
+            results.append("")
+
+    datas["MAGGIC"] = results
+
+
+def parseMAGGIC(driver, AGE, SEX, DIAB, COPD, SMOKE, NYHA):
+    driver.refresh() #SO IT DOESNT KEEP ADDING DATA TO THE PAGE
+
+    if (AGE != "nan" and SEX != "nan" and DIAB != "nan" and SMOKE != "nan" and NYHA != "nan"):
+        driver.find_element_by_css_selector('#age').send_keys(AGE)
+
+        #MUST CLICK BEFORE ENTERING GENDER STUPID DROPDOWN ANIMATIONS
+        driver.find_element_by_css_selector('#diabetic-yes' if DIAB else '#diabetic-no').click()
+        time.sleep(.25)
+
+        sexy = Select(driver.find_element_by_css_selector('#gender'))
+        sexy.select_by_value('1' if SEX else '0')
+
+        driver.find_element_by_css_selector('#copd-yes' if COPD else '#copd-no').click()
+        ########## ALL YESS SINCE NO CLOUMN IN DATA #####################
+        driver.find_element_by_css_selector('#heart-failure-yes').click()
+        #################################################################
+        driver.find_element_by_css_selector('#smoker-yes' if SMOKE else '#smoker-no' ).click()
+
+        nyhaSel = Select(driver.find_element_by_css_selector('#nyha'))
+        nyhaSel.select_by_value(NYHA)
+        print(NYHA)
+
+
+
+
+
+    return ""
+
+
+
+
+
+
+
+
+
+
+
+############################## GWTG ############################################
 def renderScoresGWTG(datas):
     driver = webdriver.Firefox() #SWITCH TO: Chrome() IF NEEDED
     driver.get(GWTG)
@@ -44,8 +131,8 @@ def renderScoresGWTG(datas):
     rows = datas.shape[0]
     results = []
 
-# NUMBER OF TIMES TO RUN, CHANGE ROWS TO SOME OTHER NUM FOR TESTS ##############
-    for i in range(rows):
+################ NUMBER OF TIMES TO RUN, CHANGE ROWS TO SOME OTHER NUM FOR TESTS
+    for i in range(12):
         res = parseGWTG(driver, str(datas['BPSYS'][i]),
                                    str(datas['BUN'][i]),
                                    str(datas['SOD'][i]),
@@ -67,7 +154,7 @@ def renderScoresGWTG(datas):
 
     datas["GWTG"] = results
 
-def grabResults(driver):
+def grabResultsGWTG(driver):
     try:
         waiter  = WebDriverWait(driver, 15).until( #TIMEOUT SET TO 15 SECONDS
             ec.visibility_of_element_located(
@@ -119,7 +206,7 @@ def parseGWTG(driver, BPSYS, BUN, SOD, AGE, HR, COPD, BLACK):
         driver.execute_script("arguments[0].scrollIntoView();", blackBtn)
         ActionChains(driver).move_to_element(blackBtn).click().perform()
 
-        return grabResults(driver)
+        return grabResultsGWTG(driver)
 
 
     return ""
